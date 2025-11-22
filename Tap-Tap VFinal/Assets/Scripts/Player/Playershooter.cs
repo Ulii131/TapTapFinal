68ba5abe6b3fd56a1e4ab5057dc0916b3ae805b7
NahuelALB <ivanalbarado8@gmail.com>
2025-11-21 22:07:31 -0300

    Implementación del Protagonista al juego
    En este commit se realizaron los cambios del personaje con la importación de blender, se agregó las animaciones, se usó el script "Player Shoot" para el disparo y escudo.
    Se inutilizaron los scripts "Hand Raiser"  y "Hand BPM"

--- Tap-Tap VFinal/Assets/Scripts/Player/Playershooter.cs
+++ Tap-Tap VFinal/Assets/Scripts/Player/Playershooter.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerShooter : MonoBehaviour
 {
@@ -8,6 +9,9 @@ public class PlayerShooter : MonoBehaviour
     [SerializeField] private GameObject bulletPrefab;
     [SerializeField] private Transform firePoint;
     [SerializeField] private float bulletSpeed = 20f;
+    [SerializeField] private Animator anim_Protagonista;
+    [SerializeField] private GameObject shield;
+    Vector3 posInicialShield;
 
     private RhythmManager rhythmManager;
 
@@ -15,6 +19,9 @@ public class PlayerShooter : MonoBehaviour
     {
         rhythmManager = FindObjectOfType<RhythmManager>();
         if (rhythmManager == null) Debug.LogError("PlayerShooter: No se encontró RhythmManager.");
+
+        //Guardamos la posición inicial del escudo
+        posInicialShield = shield.transform.localPosition;
     }
 
     void Update()
@@ -22,13 +29,44 @@ public class PlayerShooter : MonoBehaviour
         // Usar el RhythmManager para validar si el click es 'On Beat'
         if (Input.GetMouseButtonDown(1) && rhythmManager != null && rhythmManager.IsTimeToMove())
         {
+            anim_Protagonista.Play("Attack_Protagonista");
             Shoot();
+
+        }
+
+        if (Input.GetMouseButtonUp(0) && rhythmManager != null && rhythmManager.IsTimeToMove())
+        {
+            anim_Protagonista.Play("Block_Protagonista");
+            StartCoroutine(ShieldActive(0.3f));
         }
     }
 
     void Shoot()
     {
+
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
         bullet.GetComponent<PlayerBullet>().Initialize(firePoint.forward, bulletSpeed);
+
+
+    }
+
+    IEnumerator ShieldActive(float seconds)
+    {
+        shield.SetActive(true);
+
+        float initialSeconds = 0f;
+        Vector3 moveUpShield = new Vector3(0, 17, 0);
+
+        while (initialSeconds <= seconds)
+        {
+            shield.transform.localPosition = Vector3.Lerp(posInicialShield, posInicialShield + moveUpShield, initialSeconds / seconds);
+
+            initialSeconds += Time.deltaTime;
+            yield return null;
+        }
+
+        shield.transform.localPosition = posInicialShield;
+
+        shield.SetActive(false);
     }
 }
